9a5b5f40c52461aeac045e2c2081cabbec012f66
Vivek Ajay Jariwala <114449377+vjariwal@users.noreply.github.com>
2025-01-01 05:34:21 -0500

    Added Netlify dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
    vite: {
     plugins: [tailwindcss()],
   },
